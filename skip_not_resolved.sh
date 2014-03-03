89638ab0fd2c695acbaf783138953b426ce0e1a4
root <root@centosuser.(none)>
2014-03-03 20:29:47 +0600

    new scripts

---
+++ skip_not_resolved.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. /etc/init.d/functions
+
+> failed
+> blocked
+
+while read line; do
+	echo -n $line
+	/usr/bin/curl --silent --connect-timeout 2 $line &>/dev/null
+	if [ "$?" = 6 ]; then
+		echo $line >> blocked 
+		echo_success
+	else
+		echo $line >> failed
+		echo_failure
+	fi
+	echo
+done < ${1:-url.list}
