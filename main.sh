7e4c8d05ada4c10c2d00a3cd536973774c4ded1a
root <root@centosuser.(none)>
2014-07-24 17:08:04 +0600

    refactored

--- main.sh
+++ main.sh
@@ -5,16 +5,29 @@
 > failed
 > blocked
 
-while read line; do
+check() {
 	echo -n $line
-	/usr/bin/curl --silent --connect-timeout 2 $line 2>/dev/null 1>/tmp/curl.$$
-	retval=$?
-	if [ "$(</tmp/curl.$$)" != '302 Found. Site Block' -a "$retval" != '6' ]; then
-		echo $retval: $line>> failed
+	# /usr/bin/curl --silent --connect-timeout 2 $line 2>/dev/null 1>/tmp/curl.$$
+	wget -q -t 1 -T 2 $line 2>/dev/null 1>/tmp/curl.$$
+	local retval=$?
+	if [ "$retval" = '0' ]; then
+	# if [ "$(</tmp/curl.$$)" != '302 Found. Site Block' -a "$retval" != '6' ]; then
+		echo $retval: $line >> failed
 		echo_failure
 	else
+		if [ "$retval" != '4' ]; then
+			echo $retval: $line >> warning
+			echo_warning
+			echo
+			return
+		fi
 		echo $line >> blocked
 		echo_success
 	fi
 	echo
+}
+
+while read line; do
+	[ "$(pgrep -P $$ | wc -l)" -gt '10' ] && wait
+	check $line &
 done < ${1:-url.list}
