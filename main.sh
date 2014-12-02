5d1052b685d95db9e4dab6f72ab16fa8af0309ad
root <root@centosuser.(none)>
2014-12-02 15:34:54 +0500

    last changes

--- main.sh
+++ main.sh
@@ -2,32 +2,6 @@
 
 . /etc/init.d/functions
 
-> failed
-> blocked
-
-check() {
-	echo -n $line
-	# /usr/bin/curl --silent --connect-timeout 2 $line 2>/dev/null 1>/tmp/curl.$$
-	wget -q -t 1 -T 2 $line 2>/dev/null 1>/tmp/curl.$$
-	local retval=$?
-	if [ "$retval" = '0' ]; then
-	# if [ "$(</tmp/curl.$$)" != '302 Found. Site Block' -a "$retval" != '6' ]; then
-		echo $retval: $line >> var/failed
-		echo_failure
-	else
-		if [ "$retval" != '4' ]; then
-			echo $retval: $line >> var/warning
-			echo_warning
-			echo
-			return
-		fi
-		echo $line >> var/blocked
-		echo_success
-	fi
-	echo
-}
-
 while read line; do
-	[ "$(pgrep -P $$ | wc -l)" -gt '10' ] && wait
-	check $line &
-done < ${1:-var/url.list}
+	./curl $line
+done < ${1:-var/url.list} | tee /var/log/reductor.log
